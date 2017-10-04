9b15111b0804ecd9c21f11de87b50ad564c36a31
John <john@silverstripe.com>
2017-10-04 16:09:02 +1300

    Add validation when specifying Freshdesk fields

--- code/extensions/EditableFormFieldFreshdeskExtension.php
+++ code/extensions/EditableFormFieldFreshdeskExtension.php
@@ -11,4 +11,28 @@ class EditableFormFieldFreshdeskExtension extends DataExtension
         $fields->addFieldToTab('Root.Main', new TextField('FreshdeskFieldMapping', 'Freshdesk field mapping:'));        
         $fields->addFieldToTab('Root.Main', new CheckboxField('FreshdeskFieldCustom', 'Freshdesk custom field'));
     }
+
+    /*
+    * Ensure Freshdesk fields exist via API call
+    */
+    public function validate(ValidationResult $validationResult) {
+        $vaildFieldNames = false;
+
+        if ($this->owner->FreshdeskFieldMapping) {
+            if (!$vaildFieldNames) {
+                $headers = ["Content-type" => "application/json"];
+                $freshdesk = \FreshdeskAPI::create();
+                $result = $freshdesk->APICall('GET', FRESHDESK_API_BASEURL, '/api/v2/ticket_fields', $headers);
+                if ($result && $result->getStatusCode() == '200') {
+                    $validFields = json_decode($result->getBody()->getContents(), true);
+                }
+            }
+            foreach ($validFields as $field) {
+                if ($field['name'] == $this->owner->FreshdeskFieldMapping) {
+                    return $validationResult->valid();
+                }
+            }
+            return $validationResult->error($this->owner->FreshdeskFieldMapping . ' is not a valid Freshdesk field');
+        }
+    }
 }
