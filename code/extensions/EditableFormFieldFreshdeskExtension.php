259197942f3205daaa8ac59dc125998d514153b8
John <john@silverstripe.com>
2017-10-04 16:21:38 +1300

    Missed misunderstanding

--- code/extensions/EditableFormFieldFreshdeskExtension.php
+++ code/extensions/EditableFormFieldFreshdeskExtension.php
@@ -16,23 +16,23 @@ class EditableFormFieldFreshdeskExtension extends DataExtension
     * Ensure Freshdesk fields exist via API call
     */
     public function validate(ValidationResult $validationResult) {
-        $vaildFieldNames = false;
-
-        if ($this->owner->FreshdeskFieldMapping) {
-            $headers = ["Content-type" => "application/json"];
-            $freshdesk = \FreshdeskAPI::create();
-            $result = $freshdesk->APICall('GET', FRESHDESK_API_BASEURL, '/api/v2/ticket_fields', $headers);
-
-            if ($result && $result->getStatusCode() == '200') {
-                $validFields = json_decode($result->getBody()->getContents(), true);
-            }
-            
-            foreach ($validFields as $field) {
-                if ($field['name'] == $this->owner->FreshdeskFieldMapping) {
-                    return $validationResult->valid();
-                }
-            }
-            return $validationResult->error($this->owner->FreshdeskFieldMapping . ' is not a valid Freshdesk field');
+        if (!$this->owner->FreshdeskFieldMapping) {
+            return $validationResult->valid();
         }
+
+        $headers = ["Content-type" => "application/json"];
+        $freshdesk = \FreshdeskAPI::create();
+        $result = $freshdesk->APICall('GET', FRESHDESK_API_BASEURL, '/api/v2/ticket_fields', $headers);
+
+        if ($result && $result->getStatusCode() == '200') {
+            $validFields = json_decode($result->getBody()->getContents(), true);
+        }
+
+        foreach ($validFields as $field) {
+            if ($field['name'] == $this->owner->FreshdeskFieldMapping) {
+                return $validationResult->valid();
+            }
+        }
+        return $validationResult->error($this->owner->FreshdeskFieldMapping . ' is not a valid Freshdesk field');
     }
 }
