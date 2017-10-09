23cf7053df28df87b28dff8ada7228b2304228db
Jake Dale Ovenden <jakedaleweb@users.noreply.github.com>
2017-10-09 15:31:35 +1300

    Initial SSO work by John (#1)
    * Initial SSO work by John
    
    * Updated FreshdeskPage
    
    * Ability to filter list of Freshdesk tickets
    
    * Fix client dependency
    
    * Use APICall to get tickets to display
    
    * Change order of APICall params
    
    * General code clean
    
    * Freshdesk -> FreshdeskAPI
    
    * Paginate tikets
    
    * Convert status and priority to human readable values
    
    * Document humanReadable()
    
    * Clean up SSO class
    
    * move API code to it's own folder
    
    * Remove FreshdeskDomain in favor of FRESHDESK_API_BASEURL
    
    * Better logging for set-up of Freshdesk userform ticket
    
    * Initial ticket filtering work
    
    * Allow overriding of status & priority via yml
    
    * Filter form
    
    * Rename ClientFactory
    
    * Move form template to module
    
    * Check for member username on role check
    
    * Document @config
    
    * Readme updates
    
    * Clean up of SSO class
    
    * Change 'submit' to 'filter'
    
    * Ensure user is logged in for ticket generation
    
    * Remove hard coded URLs and introduce logout method
    
    * Redirect on bad SS env vars
    
    * Handle for case where the user doesn't exist in FD
    
    * Ability to move form fields out of description, into (custom) fields
    
    * Class -> class
    
    * Remove elses
    
    * Add validation when specifying Freshdesk fields
    
    * Tidy up misunderstanding
    
    * Missed misunderstanding
    
    * Remove unused var
    
    * Cleanup
    
    * Populate statuses via API, add Agent details
    
    * Unassigned agent now empty
    
    * If... else, else!
    
    * Update readme

---
+++ code/services/FreshdeskSSO.php
@@ -0,0 +1,58 @@
+<?php
+
+class FreshdeskSSO extends Controller
+{
+
+    public static $allowed_actions = [
+        'simpleLogin',
+        'simpleLogout',
+    ];
+
+    private static $freshdeskPortalRedirects = [];
+
+    public function simpleLogin()
+    {
+        if (!defined('FRESHDESK_HMAC_SECRET') && !defined('FRESHDESK_PORTAL_BASEURL'))  {
+            $this->redirect('home/');
+        }
+        
+        // Route different Portals - single instance of Freshdesk
+        $portalUrl = $this->request->getVar('host_url');
+        $freshdeskPortalRedirects = Config::inst()->get('FreshdeskSSO', 'freshdeskPortalRedirects');
+
+        if (array_key_exists($portalUrl, $freshdeskPortalRedirects)) {
+            $this->redirect($freshdeskPortalRedirects[$portalUrl].'/freshdesksso/simpleLogin?host_url='.$portalUrl);
+        } else {
+            $currentMember = \Member::currentUser();
+            if (!$currentMember || !$currentMember->exists()) {
+                return \Security::permissionFailure();
+            }
+            $this->redirect($this->getSSOUrl($currentMember->getName(),$currentMember->Email));
+        }
+    }
+
+    private function getSSOUrl($strName, $strEmail)
+    {
+        $timestamp = time();
+        $toBeHashed = $strName . FRESHDESK_HMAC_SECRET . $strEmail . $timestamp;
+        $hash = hash_hmac('md5', $toBeHashed, FRESHDESK_HMAC_SECRET);
+        return 'http://'.FRESHDESK_PORTAL_BASEURL.'/login/sso/?name='.urlencode($strName).'&email='.urlencode($strEmail).'&timestamp='.$timestamp.'&hash='.$hash;
+    }
+
+    public function simpleLogout()
+    {
+        // Route different Portals - single instance of Freshdesk
+        $portalUrl = $this->request->getVar('host_url');
+        $freshdeskPortalRedirects = Config::inst()->get('FreshdeskSSO', 'freshdeskPortalRedirects');
+
+        if (array_key_exists($portalUrl, $freshdeskPortalRedirects)) {
+            $this->redirect($freshdeskPortalRedirects[$portalUrl].'/freshdesksso/simpleLogout');
+        } else {
+            $currentMember = \Member::currentUser();
+            if ($currentMember) {
+                $currentMember->logOut();            
+            }
+        $this->redirect('home/');
+        }
+    }
+}
